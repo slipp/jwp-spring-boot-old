a3c1395d54377cd2d70476175b8537d1b94e3827
javajigi <javajigi@gmail.com>
2016-08-23 07:05:25 +0900

    JPA가 정상동작하도록 설정 변경함.

--- src/test/java/core/test/RepositoryConfiguration.java
+++ src/test/java/core/test/RepositoryConfiguration.java
@@ -1,10 +1,12 @@
 package core.test;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableAutoConfiguration
 @Configuration
 @EntityScan("next.model")
 @EnableJpaRepositories("next.repository")
